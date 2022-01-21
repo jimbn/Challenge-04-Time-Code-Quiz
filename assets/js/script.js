e33dc4cd70a70faf0505107cdf5e7828eb5b0f05
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-01-21 00:06:52 -0800

    added basic quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,142 @@
+var score = 0;
+
+
+// Question list
+let questions = [
+    {
+        id: 0, 
+        q: "Question 1",
+        o: ["answer1.1",
+            "answer1.2",
+            "answer1.3",
+            "answer1.4"
+        ],
+        a: "answer1.3"
+        
+    },
+    {   
+        id: 1,
+        q: "Question 2",
+        o: ["answer2.1", 
+            "answer2.2", 
+            "answer2.3",        
+            "answer2.4"
+           ],
+        a: "answer2.4"
+    },
+    {   
+        id: 2,
+        q: "Question 3",
+        o: ["answer3.1", 
+            "answer3.2",          
+            "answer3.3", 
+            "answer3.4" 
+           ],
+        a: "answer3.1"
+    },
+    {
+        id: 3,
+        q: "Question 4",
+        o: ["answer4.1", 
+            "answer4.2", 
+            "answer4.3", 
+            "answer4.4"       
+           ],
+        a: "answer4.4"
+    },
+]
+
+function intro () {
+    var intro = document.getElementById("intro");
+    var begin = document.querySelector(".begin-quiz");
+
+    begin.addEventListener('click', function() {
+        intro.style.display = "none";
+        quizBegin();
+    });
+};
+
+
+// Start quiz
+function quizBegin(id) {
+ 
+var quizDisplay = document.querySelector("#quiz-panel");
+quizDisplay.style.display = "block";
+
 // Getting question
-const question = document.getElementById("question");
+const question = document.querySelector("#question");
 
 // Setting question text
-question.innerText = "Hello World! Q";
+question.innerText = questions[id].q;
 
-// Getting possible answer
-const ans1 = document.getElementById("ans1");
-const ans2 = document.getElementById("ans2");
-const ans3 = document.getElementById("ans3");
-const ans4 = document.getElementById("ans4");
+// Getting anwers
+const answers = document.querySelector(".question-container");
+var ans1 = document.querySelector("#ans1");
+var ans2 = document.querySelector("#ans2");
+var ans3 = document.querySelector("#ans3");
+var ans4 = document.querySelector("#ans4");
+
 
 // Setting possible answer text
-ans1.innerText = "Hello World! 1";
-ans2.innerText = "Hello World! 2";
-ans3.innerText = "Hello World! 3";
-ans4.innerText = "Hello World! 4";
+ans1.innerText = questions[id].a[0].text;
+ans2.innerText = questions[id].a[1].text;
+ans3.innerText = questions[id].a[2].text;
+ans4.innerText = questions[id].a[3].text;
+
+// Assigning true or false value to the answers
+ans1.value = questions[id].a[0].isCorrect;
+ans2.value = questions[id].a[1].isCorrect;
+ans3.value = questions[id].a[2].isCorrect;
+ans4.value = questions[id].a[3].isCorrect;
+
+
+var result = document.getElementById("result");
+
+var selected = "";
+
+
+// When selecting answers
+ans1.addEventListener('click', function() {
+    selected = ans1.value;
+    if (selected === "true") {
+        result.innerHTML = "CORRECT!";
+        score++;
+    } else {
+        result.innerHTML = "WRONG!";
+    }
+})
+ans2.addEventListener('click', function() {
+    selected = ans2.value;
+    if (selected === "true") {
+        result.innerHTML = "CORRECT!";
+        score++;
+    } else {
+        result.innerHTML = "WRONG!";
+    }
+})
+ans3.addEventListener('click', function() {
+    selected = ans3.value;
+    if (selected === "true") {
+        result.innerHTML = "CORRECT!";
+        score++;
+    } else {
+        result.innerHTML = "WRONG!";
+    }
+})
+ans4.addEventListener('click', function() {
+    selected = ans4.value;
+    if (selected === "true") {
+        result.innerHTML = "CORRECT!";
+        score++;
+    } else {
+        result.innerHTML = "WRONG!";
+    }
+})
+
+
+};
+
+
 
 // Getting time
 var timer = document.getElementsByClassName("timer");
@@ -22,41 +144,5 @@ var timer = document.getElementsByClassName("timer");
 // Setting timer
 timer.innerText = "Countdown";
 
-const Questions = [
-    {
-        id:0,
-        q: "Question 1",
-        a: [{text: "answer1.1", isCorrect: false},
-            {text: "answer1.2", isCorrect: false},
-            {text: "answer1.3", isCorrect: true},
-            {text: "answer1.4", isCorrect: false},
-        ]
-    },
-    {
-        id:2,
-        q: "Question 2",
-        a: [{text: "answer2.1", isCorrect: false},
-            {text: "answer2.2", isCorrect: false},
-            {text: "answer2.3", isCorrect: true},
-            {text: "answer2.4", isCorrect: false},
-        ]
-    },
-    {
-        id:3,
-        q: "Question 3",
-        a: [{text: "answer3.1", isCorrect: false},
-            {text: "answer3.2", isCorrect: true},
-            {text: "answer3.3", isCorrect: false},
-            {text: "answer3.4", isCorrect: false},
-        ]
-    },
-    {
-        id:4,
-        q: "Question 4",
-        a: [{text: "answer4.1", isCorrect: false},
-            {text: "answer4.2", isCorrect: false},
-            {text: "answer4.3", isCorrect: false},
-            {text: "answer4.4", isCorrect: true},
-        ]
-    },
-]
+
+intro();
